34fd2e9390830949a7bcad11c731b2928df3e16d
Stephen Blum <blum.stephen@gmail.com>
2025-04-14 15:04:36 -0700

    new build script ready.

--- backend/build.sh
+++ backend/build.sh
@@ -1,52 +1,27 @@
 #!/bin/sh
-DEVELOPER=$( sed -e 's/"/\\"/g' prompt/developer.txt | tr -s '\n' '\\\n' | sed 's/ \+/ /g' | sed -e "s/[‘’']/\\'/g" )
-PROMPT=$(    sed -e 's/"/\\"/g' prompt/user.md       | tr -s '\n' '\\\n' | sed 's/ \+/ /g' | sed -e "s/[‘’']/\\'/g" )
-GAMEDATA=$(  sed -e 's/"/\\"/g' game-data.js         | tr -s '\n' '\\\n' | sed 's/ \+/ /g' | sed -e "s/[‘’']/\\'/g" )
 
 echo 
 echo "Building index.js... "
 echo "it will take 5 minutes... 🚧 🚀 🚧 "
 echo 
 
-DATA=$(echo '{
-  "model": "o1-pro",
-  "input": [
-    {
-      "role": "developer",
-      "content": [
-        {
-          "type": "input_text",
-          "text": "'"$DEVELOPER"'"
-        }
-      ]
-    },
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "input_text",
-          "text": "'"$PROMPT"' game-data.js: '"$GAMEDATA"'"
-        }
-      ]
-    }
+jq -n --rawfile developer prompt/developer.txt --rawfile user prompt/user.md \
+'{
+  model: "o1-pro",
+  input: [
+    { role: "developer", content: [{ type: "input_text", text: $developer }]},
+    { role: "user", content: [{ type: "input_text", text: $user }]}
   ],
-  "text": {
-    "format": {
-      "type": "text"
-    }
-  },
-  "reasoning": {
-    "effort": "medium"
-  },
-  "tools": [],
-  "store": true
-}')
-
-curl -Ls https://api.openai.com/v1/responses \
+  text: { format: { type: "text" } },
+  reasoning: { effort: "medium" },
+  tools: [],
+  store: true
+}' | curl -Ls https://api.openai.com/v1/responses \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d "$DATA" | jq -r '.output[] | select(.type == "message") | .content[] | select(.type == "output_text").text' \
-  > index.js
+  -d @- \
+| jq -r '.output[] | select(.type == "message") | .content[] | select(.type == "output_text").text' \
+> index.js
 
 cat index.js > revisions/index-$(date +%Y%m%d%H%M%S).js
 
